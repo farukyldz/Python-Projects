344a186658be7d6fdcdd0d07b969826e7b55671d
ÖMER FARUK YILDIZ <127441279+farukyldz@users.noreply.github.com>
2025-10-04 15:46:31 +0300

    boy_kitle_endeksi

---
+++ boy_kitle_endeksi.py
@@ -0,0 +1,8 @@
+#boy kitle endeksi
+
+boy = float(input("Lütfen boyunuzu m olarak giriniz:"))
+kilo = float(input("Lütfen kilonuzu kg olarak giriniz:"))
+
+endeks = kilo / (boy*boy)
+
+print("Boy kilo endeksiniz:",endeks )
