dc856af1f0d1d75b50f35cadd3afeba2a3fb5a1a
ÖMER FARUK YILDIZ <127441279+farukyldz@users.noreply.github.com>
2026-05-12 10:56:44 +0300

    body_mass_endeks.py
    How fit is your body?

---
+++ body_mass_endeks.py
@@ -0,0 +1,8 @@
+#body mass index
+
+height = float(input("Please enter your height in metres:"))
+weight = float(input("Please enter your weight in kilograms:"))
+
+endeks = weight / (height*height)
+
+print("Your body mass index:",endeks )
