ab887c6e607d8b61e378c81397594466e356898f
ÖMER FARUK YILDIZ <127441279+farukyldz@users.noreply.github.com>
2026-05-12 11:24:21 +0300

    to do app

---
+++ to_do_app.py
@@ -0,0 +1,43 @@
+tasks = []
+
+while True:
+    print("\n 1- Add Task")
+    print("2- Delete Task")
+    print("3- List Tasks")
+    print("4- Exit")
+
+    choise = input("your choise: ")
+
+    if choise== "1":
+        task = input("Enter the task: ")
+        tasks.append(task)
+        print("task added.")
+        
+        # --- Let’s display the list after the addition has been made ---
+        print("\nYour Current Task List:")
+        for i, g in enumerate(tasks, start=1):
+            print(f"{i} - {g}")
+        # --------------------------------------------------
+
+    elif choise == "2":
+        task = input("Enter the task to delete: ")
+        if task in tasks:
+            tasks.remove(task)
+            print("The task has been deleted.")
+        else:
+            print("No task found.")
+            
+    elif choise == "3":
+        print("\nTasks:")
+        if not tasks: 
+            print("The list is currently empty.")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                print(i, "-", task)
+                
+    elif choise == "4":
+        print("Program kapatıldı.")
+        break
+    else:
+        print("Geçersiz seçim.")
+        
